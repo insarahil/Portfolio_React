6b3ade60c64462173c9dd7f0c2a8c58df9641e99
insarahil <insarahilkhan@gmail.com>
2022-01-20 19:00:12 +0500

    About Me Page

--- client/src/utilities/commonUtils.js
+++ client/src/utilities/commonUtils.js
@@ -1,9 +1,14 @@
 import Home from "../components/home/Home";
+import AboutMe from "../components/aboutme/AboutMe";
 
 export const TotalScreen = [
   {
     screen_name: "Home",
     components: Home,
+  },
+  {
+    screen_name: "AboutMe",
+    components: AboutMe,
   },
 ];
 export const GetScreenIndex = (screen_name) => {
