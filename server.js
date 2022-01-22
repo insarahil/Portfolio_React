7717acf790c383f9cba577da14392ca65f95a4d7
insarahil <insarahilkhan@gmail.com>
2022-01-22 18:57:17 +0500

    Final

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require("dotenv").config;
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const contactRoute = require("./route/contactRoute");
 
 const app = express();
 
 // Create Middilware
 app.use(express.json());
 app.use(cors());
+app.use("/", contactRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log("Serever listening to port 5000"));
